4bcff34b9f981a991e750fb4e9e3f26b8436dc4a
hello-kozloff <hello.kozloff@gmail.com>
2022-07-31 20:07:03 +0300

    use search

--- src/components/Countries/Countries.tsx
+++ src/components/Countries/Countries.tsx
@@ -1,27 +1,44 @@
 import * as React from 'react'
 import * as Styled from './styled'
+import { debounce } from 'lodash'
 import { Container } from 'components/Container'
-import { Card } from './components'
+import { Card, Search } from './components'
 import { CountryObject } from 'types/country'
 
 export const Countries = () => {
   const [countries, setCountries] = React.useState<CountryObject[]>([])
   const [loading, setLoading] = React.useState<boolean>(true)
+  const [search, setSearch] = React.useState<string>('')
 
   React.useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-      .then((response) => response.json())
-      .then((response) => setCountries(response))
-      .finally(() => setLoading(false))
-  }, [])
+    setLoading(true)
 
-  console.log(countries)
+    const path = !search.length
+      ? 'https://restcountries.com/v3.1/all'
+      : `https://restcountries.com/v3.1/name/${search}`
+
+    fetch(path)
+      .then((response) => response.json())
+      .then((response) => {
+        if ('status' in response) return setCountries([])
+        setCountries(response)
+      })
+      .finally(() => setLoading(false))
+  }, [search])
+
+  const handleSearch = React.useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value)
+  }, [])
 
   return (
     <Container>
       <Styled.Countries>
-        <Styled.Title>List of countries:</Styled.Title>
+        <Styled.Header>
+          <Styled.Title>List of countries</Styled.Title>
+          <Search onChange={debounce(handleSearch, 200)} />
+        </Styled.Header>
         {loading && 'Loading countries...'}
+        {!loading && !!search.length && !countries.length && `Country ${search} not found!`}
         {!loading && countries.map((country, index) => <Card key={index} {...country} />)}
       </Styled.Countries>
     </Container>
