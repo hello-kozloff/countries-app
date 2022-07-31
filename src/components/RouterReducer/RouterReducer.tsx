bb196e10f816b25d83f5e6e2e87f601171acb0db
hello-kozloff <hello.kozloff@gmail.com>
2022-07-31 04:22:26 +0300

    add not found reduce

--- src/components/RouterReducer/RouterReducer.tsx
+++ src/components/RouterReducer/RouterReducer.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { useRoute } from 'react-router5'
-import * as routes from 'router/routes'
+import * as routes from '../../router/routes'
 
 export const RouterReducer = () => {
   const { route } = useRoute()
@@ -10,5 +10,7 @@ export const RouterReducer = () => {
       return <h1>Welcome to countries page</h1>
     case routes.Country.name:
       return <h1>Welcome to country detail page</h1>
+    default:
+      return <h1>Not found content</h1>
   }
 }
