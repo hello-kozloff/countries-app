62d45bd2ee467d5d4ff9e95b9b1a6b81862ae5f8
hello-kozloff <hello.kozloff@gmail.com>
2022-07-31 05:41:34 +0300

    update layout

--- src/components/Layout/components/Header/styled.ts
+++ src/components/Layout/components/Header/styled.ts
@@ -1,3 +1,8 @@
 import styled from 'styled-components'
 
-export const Header = styled.div``
+export const Header = styled.header`
+  padding: 16px 32px;
+  background-color: var(--plate-color);
+`
+
+export const ProjectName = styled.span``
