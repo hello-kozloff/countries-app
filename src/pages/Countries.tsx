68259392233354fe6218398f66d10dca26588678
hello-kozloff <hello.kozloff@gmail.com>
2022-07-31 05:40:36 +0300

    remove layout

--- src/pages/Countries.tsx
+++ src/pages/Countries.tsx
@@ -1,15 +1,12 @@
 import * as React from 'react'
 import { useRoute } from 'react-router5'
 import { Page } from 'components/Page'
-import { Layout } from 'components/Layout'
 
 export const Countries = () => {
   const { route } = useRoute()
   return (
     <Page name={route.name}>
-      <Layout>
-        <h1>Hello countries page!</h1>
-      </Layout>
+      <h1>Hello countries page!</h1>
     </Page>
   )
 }
--- src/pages/Country.tsx
+++ src/pages/Country.tsx
@@ -1,15 +1,12 @@
 import * as React from 'react'
 import { useRoute } from 'react-router5'
 import { Page } from 'components/Page'
-import { Layout } from 'components/Layout'
 
 export const Country = () => {
   const { route } = useRoute()
   return (
     <Page name={route.name}>
-      <Layout>
-        <h1>View detail of the country!</h1>
-      </Layout>
+      <h1>View detail of the country!</h1>
     </Page>
   )
 }
